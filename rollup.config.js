710cd0e9f0a75d8cfd96f5aee9dea155d0dade09
Yinxiong <languid.jeremy@gmail.com>
2017-09-08 07:54:16 +0800

    init

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Yinxiong on 2016/11/20.
+ */
+
+import babel from 'rollup-plugin-babel'
+
+export default {
+  input: 'src/main.js',
+  output: {
+    file: 'dist/main.js',
+    format: 'iife'
+  },
+  name: 'main',
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ]
+}
